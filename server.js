97b6d78f349d900302c007d43037a4900e62e82b
Deepali Garg <deepaligarg.chd@gmail.com>
2021-03-04 15:45:19 -0500

    added server, gitignore, npm i

---
+++ server.js
@@ -0,0 +1,16 @@
+// DEPENDENCIES
+const express = require('express')
+//EXPRESS CONFIGURATION
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
